77753ef6c9e3a1ea6d0ddb1e40a2aecafc693e42
mkk <mk1210k@gmail.com>
2024-04-11 16:42:09 +0900

    chore: ESLint, Prettier 설정 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,10 +5,12 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'react', 'prettier'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
