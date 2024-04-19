f71a64e0a28b1d78907668c77785fd73c655a562
mkk <mk1210k@gmail.com>
2024-04-20 01:38:43 +0900

    style: 전역 폰트 설정 추가

--- src/styles/global-styles.ts
+++ src/styles/global-styles.ts
@@ -4,9 +4,17 @@ import { normalize } from 'styled-normalize'
 const GlobalStyle = createGlobalStyle`
   ${normalize}
 
+  @font-face {
+    font-family: 'NanumSquareNeo-Variable';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_11-01@1.0/NanumSquareNeo-Variable.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+}
+
   html,
   body {
     overflow: hidden;
+    font-family: 'NanumSquareNeo-Variable', sans-serif;
   }
 
   * {
