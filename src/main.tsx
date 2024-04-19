be76892f41a308ee7db8d1cf2552627e59385f4d
mkk <mk1210k@gmail.com>
2024-04-20 01:37:46 +0900

    style: 전역 스타일 추가 및 적용, 색상 theme 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { ThemeProvider } from 'styled-components'
+import { theme } from '@/styles/theme'
+import GlobalStyle from '@/styles/global-styles'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <App />
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+  </ThemeProvider>
 )
---
+++ src/styles/theme.ts
@@ -0,0 +1,10 @@
+import { DefaultTheme } from 'styled-components'
+
+export const theme: DefaultTheme = {
+  basicWidth: '320px',
+
+  color: {
+    main: '#3c5afe',
+    sub: '#ff6e40'
+  }
+}
