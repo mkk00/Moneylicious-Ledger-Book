bec4c8565bd3063c91e3c4dc36dfb88976450c1d
mkk <mk1210k@gmail.com>
2024-04-21 01:07:32 +0900

    feat: react-router 설치 및 기본 path 적용

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,20 @@ import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
 import { theme } from '@/styles/theme'
 import GlobalStyle from '@/styles/global-styles'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import Home from '@/pages/Home'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <ThemeProvider theme={theme}>
     <GlobalStyle />
+    <RouterProvider router={router} />
   </ThemeProvider>
 )
