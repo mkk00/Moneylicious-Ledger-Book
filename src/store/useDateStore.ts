715070f0b15bb0df2f36527e3a3cbf584f0ed141
mkk <mk1210k@gmail.com>
2024-05-01 22:58:33 +0900

    feat: 상태 관리 라이브러리 zustand 추가 및 날짜 선택 store 생성

---
+++ src/store/useDateStore.ts
@@ -0,0 +1,22 @@
+import { create } from 'zustand'
+
+interface DateProps {
+  selectedDate: Date | null
+  setSelectedDate: (date: Date) => void
+  formatSelectedDate: (date: Date) => string | null
+}
+
+const useDateStore = create<DateProps>(set => ({
+  selectedDate: null,
+  setSelectedDate: date => set({ selectedDate: date }),
+  formatSelectedDate: date => {
+    if ({ date }.date) {
+      const year = date.getFullYear()
+      const month = String(date.getMonth() + 1).padStart(2, '0')
+      const day = String(date.getDate()).padStart(2, '0')
+      return `${year}/${month}/${day}`
+    } else return null
+  }
+}))
+
+export default useDateStore
