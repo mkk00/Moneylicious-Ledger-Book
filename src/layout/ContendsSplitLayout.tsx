8c90e0b1dd51e6471264402ad372c6e3a2d1e797
mkk <mk1210k@gmail.com>
2024-05-30 15:41:33 +0900

    feat: Layout 컴포넌트 모바일 반응형

--- src/layout/ContendsSplitLayout.tsx
+++ src/layout/ContendsSplitLayout.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from 'react'
 import styled from 'styled-components'
+import { useResponsive } from '@/hook/useMediaQuery'
 
 const ContendsSplitLayout = ({
   left,
@@ -12,8 +13,9 @@ const ContendsSplitLayout = ({
   right?: ReactNode
   flex?: number[]
 }) => {
+  const { isNotMobile } = useResponsive()
   return (
-    <Container>
+    <Container $isNotMobile={isNotMobile}>
       {left && <Left $flex={flex?.[0]}>{left}</Left>}
       {center && <Center $flex={flex?.[1]}>{center}</Center>}
       {right && <Right $flex={center ? flex?.[2] : flex?.[1]}>{right}</Right>}
@@ -23,9 +25,11 @@ const ContendsSplitLayout = ({
 
 export default ContendsSplitLayout
 
-const Container = styled.div`
-  display: flex;
+const Container = styled.div<{ $isNotMobile: boolean }>`
   width: 100%;
+  display: flex;
+  flex-direction: ${({ $isNotMobile }) => ($isNotMobile ? 'row' : 'column')};
+  gap: ${({ $isNotMobile }) => ($isNotMobile ? '0' : '30px')};
 `
 
 const Left = styled.section<{ $flex?: number }>`
