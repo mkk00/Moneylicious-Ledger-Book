877d7fa87f883bb7bc20d1c39378d67176c1598a
mkk <mk1210k@gmail.com>
2024-07-24 01:40:07 +0900

    feat: 대시보드 페이지 가계부 목록 조회 로직 추가

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,34 @@
+import MonthlyChart from '@/components/dashBoard/MonthlyChart'
 import PageLayout from '@/layout/PageLayout'
+import { supabase } from '@/supabaseconfig'
+import { useEffect, useState } from 'react'
+import { LedgerProps } from '@/interface/LedgerProps'
 
 const Dashboard = () => {
+  const [ledgerData, setLedgerData] = useState<LedgerProps[] | null>(null)
+
+  const getLederData = async () => {
+    try {
+      const { data, error } = await supabase.from('ledger').select('*')
+
+      if (data) {
+        setLedgerData(data)
+        ledgerData && console.log(ledgerData)
+      } else {
+        setLedgerData(null)
+      }
+      if (error) throw error
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    getLederData()
+  }, [])
   return (
     <PageLayout>
-      <>대시보드</>
+      <MonthlyChart ledgerData={ledgerData} />
     </PageLayout>
   )
 }
